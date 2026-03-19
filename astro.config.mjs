6b2cdfd7072435e6edd9f0e3f6fe2b233eb15a0a
perebvilalta <perebvilalta@gmail.com>
2026-03-19 12:14:22 +0100

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@astrojs/react';
+import markdoc from '@astrojs/markdoc';
+import keystatic from '@keystatic/astro';
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'server',
+  adapter: cloudflare(),
+  integrations: [react(), markdoc(), keystatic()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
